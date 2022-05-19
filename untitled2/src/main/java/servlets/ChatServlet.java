4332b6765901b9e2e4d87b47b4255d6988c59121
Semyon Ageyko <petukhin1992@mail.ru>
2022-05-19 17:14:29 +0300

    feat: final var

---
+++ untitled2/src/main/java/servlets/ChatServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import bsu.rfe.java.group6.lab8.DMUKHOVSKIY.var9A.ChatMessage;
+import bsu.rfe.java.group6.lab8.DMUKHOVSKIY.var9A.ChatUser;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ChatServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    // Карта текущих пользователей
+    protected HashMap<String, ChatUser> activeUsers;
+    // Список сообщений чата
+    protected ArrayList<ChatMessage> messages;
+
+    @SuppressWarnings("unchecked")
+    public void init() throws ServletException {
+// Вызвать унаследованную от HttpServlet версию init()
+        super.init();
+// Извлечь из контекста карту пользователей и список сообщений
+        activeUsers = (HashMap<String, ChatUser>)
+                getServletContext().getAttribute("activeUsers");
+        messages = (ArrayList<ChatMessage>)
+                getServletContext().getAttribute("messages");
+// Если карта пользователей не определена ...
+        if (activeUsers == null) {
+// Создать новую карту
+            activeUsers = new HashMap<String, ChatUser>();
+// Поместить еѐ в контекст сервлета,
+// чтобы другие сервлеты могли до него добраться
+            getServletContext().setAttribute("activeUsers",
+                    activeUsers);
+        }
+// Если список сообщений не определѐн ...
+        if (messages == null) {
+// Создать новый список
+            messages = new ArrayList<ChatMessage>(100);
+// Поместить его в контекст сервлета,
+// чтобы другие сервлеты могли до него добрать
+            getServletContext().setAttribute("messages", messages);
+        }
+    }
+}
---
+++ untitled2/src/main/java/servlets/MessageListServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import bsu.rfe.java.group6.lab8.DMUKHOVSKIY.var9A.ChatMessage;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MessageListServlet extends ChatServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+// Установить кодировку HTTP-ответа UTF-8
+        response.setCharacterEncoding("utf8");
+// Получить доступ к потоку вывода HTTP-ответа
+        PrintWriter pw = response.getWriter();
+// Записть в поток HTML-разметку страницы
+        pw.println("<html><head><meta http-equiv='Content-Type' " +
+                "content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
+        pw.println("<body>");
+// В обратном порядке записать в поток HTML-разметку для каждого сообщения
+        ChatMessage aaMessage = messages.get(0);
+        pw.println("<div>Пользователь <strong>" + aaMessage.getAuthor().getName() + "</strong> пришёл в чат" + "</div>");
+
+        for (int i = messages.size() - 1; i >= 0; i--) {
+            ChatMessage aMessage = messages.get(i);
+
+
+            pw.println("<div><strong>" + aMessage.getAuthor().getName()
+                    + "</strong>: " + aMessage.getMessage() + "</div>");
+
+        }
+        pw.println("</body></html>");
+    }
+
+}
